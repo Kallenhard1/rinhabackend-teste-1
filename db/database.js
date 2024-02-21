340072551101f13bf8b78b8047872258cf9bdddd
Mario Lucas da Silva Barbosa <30977081+Kallenhard1@users.noreply.github.com>
2024-02-21 14:39:04 +0000

    Adding connection with postgres database

--- db/database.js
+++ db/database.js
@@ -6,6 +6,23 @@ const pessoas = [
   { id: 5, name: "Mario" },
 ];
 
-module.exports = {
-  pessoas,
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client();
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query: query,
+  pessoas: pessoas,
 };
