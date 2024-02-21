93d45a2aec6af357c12cde191acf491d0dc9d39a
Mario Lucas da Silva Barbosa <30977081+Kallenhard1@users.noreply.github.com>
2024-02-21 14:27:24 +0000

    creating the first CRUD in index.js and modifyng docker-compose and package.json

--- app/index.js
+++ app/index.js
@@ -10,16 +10,90 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World\n");
-});
-
-// Teste, pegando do banco, só um exemplo usando objetos no banco
 app.get("/pessoas", (req, res) => {
   const pessoas = db.pessoas;
-  res.status(200).json({
-    pessoas: pessoas,
-  });
+  try {
+    res.status(200).json({
+      pessoas: pessoas,
+    });
+  } catch (err) {
+    res.status(401).json({
+      message: "Não foi possivel acessar pessoas",
+      error: err,
+    });
+  }
+});
+
+app.get("/pessoas/:id", (req, res) => {
+  const { id } = req.params;
+  const pessoa = db.pessoas.filter((obj) => obj.id === Number(id));
+  try {
+    res.status(200).json({
+      id: id,
+      title: pessoa[0].name,
+    });
+  } catch (err) {
+    res.status(401).json({
+      message: "Não foi possivel acessar pessoa",
+      error: err,
+    });
+  }
+});
+
+app.post("/pessoas", (req, res) => {
+  const { id, name } = req.body;
+  const body = {
+    id: id,
+    name: name,
+  };
+
+  try {
+    db.pessoas.push(body);
+    res.status(200).json({
+      success: true,
+      message: "Pessoa cadastrada com sucesso!",
+    });
+  } catch (err) {
+    res.status(401).json({
+      success: false,
+      message: "Não foi possivel cadastrar pessoa",
+    });
+  }
+});
+
+app.put("/pessoas/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const pessoa = db.pessoas.filter((obj) => obj.id === Number(id));
+  try {
+    pessoa[0] = name;
+    res.status(200).json({
+      success: true,
+      message: "Pessoa atualizada com sucesso!",
+    });
+  } catch (err) {
+    res.status(401).json({
+      success: false,
+      message: "Não foi possivel atualizar pessoa",
+    });
+  }
+});
+
+app.delete("pessoas/:id", (req, res) => {
+  const { id } = req.params;
+  const pessoa = db.pessoas.filter((obj) => obj.id === Number(id));
+  try {
+    db.pessoas.pop(pessoa);
+    res.status(200).json({
+      success: true,
+      message: "Pessoa deletada com sucesso!",
+    });
+  } catch (err) {
+    res.status(401).json({
+      success: false,
+      message: "Não foi possivel deletar pessoa",
+    });
+  }
 });
 
 app.listen(port, () => {
