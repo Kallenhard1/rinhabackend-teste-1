69a561684b04b5be2070e9847bc0b72d0276382c
Mario Lucas da Silva Barbosa <30977081+Kallenhard1@users.noreply.github.com>
2024-02-23 15:04:22 +0000

    Fixing postgres connection and adding tab init.sql le pessoas to docker volume

--- db/database.js
+++ db/database.js
@@ -2,11 +2,11 @@ const { Client } = require("pg");
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTGRES_HOST || "localhost",
-    port: process.env.POSTGRES_PORT || 5432,
-    user: process.env.POSTGRES_USER || "root",
-    database: process.env.POSTGRES_DB || "rinhadb",
-    password: process.env.POSTGRES_PASSWORD || "local_password",
+    host: "localhost",
+    port: 5432,
+    user: "root",
+    database: "rinhadb",
+    password: "local_password",
   });
   try {
     await client.connect();
